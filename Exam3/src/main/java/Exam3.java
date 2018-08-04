5ede6d0140dd34e65258362275c7285be1030676
lisiyu <18392955246@163.com>
2018-08-04 13:56:48 +0800

    修改

--- Exam3/src/main/java/Exam3.java
+++ Exam3/src/main/java/Exam3.java
@@ -1,13 +1,8 @@
 import java.util.List;
 import java.util.ArrayList;
-import java.util.Vector;
-import java.util.Set;
-import java.util.HashSet;
 import java.util.Map;
 import java.util.HashMap;
-import java.util.Vector;
 import java.util.Collections;
-import java.util.Comparator;
 public class Exam3 {
     public static void main(String args[]) {
         List<Integer> list = new ArrayList<Integer>();
